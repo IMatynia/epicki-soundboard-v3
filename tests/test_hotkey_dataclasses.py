5f9434271ede46afb7798384906978eb101ce641
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-06-23 17:44:24 +0200

    hotkey list tests

--- tests/test_hotkey_dataclasses.py
+++ tests/test_hotkey_dataclasses.py
@@ -1,19 +1,58 @@
 from src.audio_hotkey import AudioHotkey, AudioHotkeyList, HotkeyCollisionError
+from src.keyboard_hotkeys import Key
 import pytest
 
 
 def test_simple_hotkey():
-    hk = AudioHotkey(set(["a", "b", "c"]), "epic_battle_music.ogg", 3)
+    keys = set([
+        Key("a", 1),
+        Key("b", 2),
+        Key("c", 3)
+    ])
+    hk = AudioHotkey(keys, "epic_battle_music.ogg", 3)
     assert hk.get_filename() == "epic_battle_music.ogg"
-    assert hk.get_keys() == set(["a", "b", "c"])
+    assert hk.get_keys() == keys
     assert hk.get_page() == 3
+
+
+def test_hotkey_compare():
+    keys1 = set([
+        Key("a", 1),
+        Key("b", 2),
+        Key("c", 3)
+    ])
+
+    keys2 = set([
+        Key("g", 6),
+        Key("e", 5),
+        Key("f", 4)
+    ])
+
+    hk1 = AudioHotkey(keys1, "bingus.ogg", 0)
+    hk2 = AudioHotkey(keys2, "bingus.ogg", 1)
+
+    assert not hk1 == hk2
+
+    hk2.set_keys(keys1)
+    assert not hk1 == hk2
+
+    hk2.set_page(0)
+    assert hk1 == hk2
+
+    hk2.set_filename("spoingus.ogg")
+    assert hk1 == hk2
 
 
 def test_simple_hotkey_list():
     hkl = AudioHotkeyList(100)
     assert hkl.get_max_pages() == 100
 
-    hk = AudioHotkey(set(["a", "b", "c"]), "epic_battle_music.ogg", 3)
+    keys = set([
+        Key("a", 1),
+        Key("b", 2),
+        Key("c", 3)
+    ])
+    hk = AudioHotkey(keys, "epic_battle_music.ogg", 3)
     hkl.add_hotkey(hk)
     assert hkl.check_collision(hk)
     assert hkl.get_page(3) == [hk]
@@ -28,10 +67,14 @@ def test_simple_hotkey_list():
 
 def test_save_load_purge_list():
     init_list = [
-        {"keys": ["*", "1"], "filename":"sogga.ogg", "page":0},
-        {"keys": ["*", "2"], "filename":"floppa.ogg", "page":0},
-        {"keys": ["*", "3"], "filename":"spingus.ogg", "page":0},
-        {"keys": ["*", "1"], "filename":"sogga.ogg", "page":1}
+        {"keys": [{"name": "*", "scan_code": 1}, {"name": "1",
+                                                  "scan_code": 2}], "filename": "sogga.ogg", "page": 0},
+        {"keys": [{"name": "*", "scan_code": 1}, {"name": "2",
+                                                  "scan_code": 3}], "filename": "floppa.ogg", "page": 0},
+        {"keys": [{"name": "*", "scan_code": 1}, {"name": "3",
+                                                  "scan_code": 4}], "filename": "spingus.ogg", "page": 0},
+        {"keys": [{"name": "*", "scan_code": 1}, {"name": "1",
+                                                  "scan_code": 2}], "filename": "sogga.ogg", "page": 1}
     ]
 
     hkl = AudioHotkeyList()
@@ -45,5 +88,8 @@ def test_save_load_purge_list():
     hkl.purge_data()
     assert len(hkl.get_page(0)) == 0
     assert len(hkl.get_page(1)) == 0
-    hkl.add_hotkey(AudioHotkey(set(["*", "1"]), "sogga.ogg", 0))
-    assert hkl.get_page(0) == [AudioHotkey(set(["*", "1"]), "sogga.ogg", 0)]
+    hkl.add_hotkey(AudioHotkey(
+        set([Key("*", 1), Key("1", 2)]), "sogga.ogg", 0))
+    page_0 = hkl.get_page(0)
+    assert page_0 == [AudioHotkey(
+        set([Key("*", 1), Key("1", 2)]), "sogga.ogg", 0)]
