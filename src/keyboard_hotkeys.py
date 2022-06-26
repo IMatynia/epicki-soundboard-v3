d62b152f1e0714a7faf04306eb7510916f1f5140
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-06-26 23:42:02 +0200

    keyboard hotkeys module now uses pynput

--- src/keyboard_hotkeys.py
+++ src/keyboard_hotkeys.py
@@ -1,7 +1,7 @@
-from logging import info
-from time import sleep, time
-import keyboard
+import pynput
 from bitarray import bitarray
+from logging import info
+# TODO: put these classes into separate files
 
 
 class Key:
@@ -24,7 +24,7 @@ class Key:
         return f"{self.name} ({self.scan_code})"
 
     def __hash__(self) -> int:
-        return hash(self.name) + hash(self.scan_code)
+        return hash((self.name, self.scan_code))
 
     def __eq__(self, other) -> bool:
         return other.name == self.name and other.scan_code == self.scan_code
@@ -36,90 +36,79 @@ class Key:
 def keys_to_string(key_set):
     return " + ".join([key.name for key in sorted(list(key_set))])
 
-
-class HoldYourHorses:
-    def __init__(self, delay=10) -> None:
-        self._delay = delay
-        self._wait_until = [0] * 256
-
-    def try_pressing_again(self, scan_code):
-        now = time()
-        can_continue = False
-        if now > self._wait_until[scan_code]:
-            can_continue = True
-
-        self._wait_until[scan_code] = now + self._delay/1000
-        return can_continue
-
-
-class KeyScanner:
-    _scan_hook_running = False
-    _n_of_pressed_keys = 0
-    _all_keys = set()
-    _scan_hook = None
-    _awaiting = HoldYourHorses()
+# TODO: make this work with a callback ("on scan complete etc")
+class HotkeyScanner:
+    _current_bitset = None
+    _key_hook = None
+    _scanned_keys = None
 
     @staticmethod
-    def _press_callback(event):
-        new_key = Key(event.name, event.scan_code)
-        if event.event_type == "down" \
-                and new_key not in KeyScanner._all_keys \
-                and KeyScanner._awaiting.try_pressing_again(event.scan_code):
-            info(
-                f"DOWN {event.scan_code} | Currently pressed: {KeyScanner._n_of_pressed_keys}")
-            KeyScanner._n_of_pressed_keys += 1
-            KeyScanner._all_keys.add(new_key)
-        elif KeyScanner._awaiting.try_pressing_again(event.scan_code):
-            info(
-                f"UP {event.scan_code} | Currently pressed: {KeyScanner._n_of_pressed_keys}")
-            KeyScanner._n_of_pressed_keys -= 1
-
-        if KeyScanner._n_of_pressed_keys <= 0:  # User stopped pressing any buttons, scan complete
-            KeyScanner._scan_hook_running = False
-            keyboard.unhook(KeyScanner._scan_hook)
-        return
+    def scan_keys_until_release_all():
+        HotkeyScanner._scanned_keys = set()
+        HotkeyScanner._current_bitset = bitarray(2**8)
+        HotkeyScanner._current_bitset.setall(0)
+        HotkeyScanner._key_hook = pynput.keyboard.Listener(
+            HotkeyScanner._keyboard_hook_on_press,
+            HotkeyScanner._keyboard_hook_on_release
+        )
+        HotkeyScanner._key_hook.start()
+        HotkeyScanner._key_hook.join()
+        return HotkeyScanner._scanned_keys
 
     @staticmethod
-    def get_keys():
-        KeyScanner._scan_hook_running = True
-        KeyScanner._scan_hook = keyboard.hook(KeyScanner._press_callback, True)
+    def _keyboard_hook_on_press(key):
+        if isinstance(key, pynput.keyboard.Key):
+            code = key.value.vk
+            name = key.name
+        else:
+            code = key.vk
+            name = key.char
 
-        timeout = 30
-        while KeyScanner._scan_hook_running and timeout > 0:
-            sleep(0.2)
-            timeout -= 0.2
+        # Numpad key
+        if 96 <= code <= 105:
+            name = f"Num {code - 96}"
 
-        if timeout <= 0:
-            raise Exception("Hotkey scanning timeout!")
+        if name is None:
+            name = f"key{code}"
 
-        detail = " ".join([str(key) for key in KeyScanner._all_keys])
-        info(f"Scanned keys: {detail}")
-        return KeyScanner._all_keys
+        if not HotkeyScanner._current_bitset[code]:
+            HotkeyScanner._scanned_keys.add(Key(name, code))
+            HotkeyScanner._current_bitset[code] = True
+            info(
+                f"New press of {name}/{code}\n{HotkeyScanner._current_bitset}")
+        return True
+
+    @staticmethod
+    def _keyboard_hook_on_release(key):
+        return False
 
 
-def scan_pressed_keys():
-    return KeyScanner.get_keys()
-
-
-class HotkeyCallback:
-    def __init__(self, key_bitmap, callback, args) -> None:
+class KeyboardHotkeyCallback:
+    def __init__(self, keys, callback, args) -> None:
         """Data class storing information about a hotkey combination
 
         Args:
-            key_bitmap (bitarray): stores which keycodes should be pressed
-                to activate callback
+            keys (iterable): keys to press to activate callback
             callback (function): the callback to run
             args (list): argument list to run callback with
         """
-        self.key_bitmap = key_bitmap
+        self.key_bitmap = KeyboardHotkeyCallback.keys_to_bitset(keys)
         self.callback = callback
         self.args = args
 
     def run(self):
         self.callback(*self.args)
 
+    @staticmethod
+    def keys_to_bitset(keys):
+        bitmask = bitarray(2**8)
+        bitmask.setall(0)
+        for key in keys:
+            bitmask[key.scan_code] = True
+        return bitmask
 
-class HotkeyManager:
+
+class HotkeyListener:
     """Static class managing hotkeys and keyboard hook.
     I implemented my own hotkey hooker because the default one from keyboard lib
     would only work if speciffically the given combination is provided.
@@ -130,18 +119,21 @@ class HotkeyManager:
         _type_: _description_
     """
     _callbacks = None
-    _bitset = None
+    _current_bitset = None
     _key_hook = None
     _enabled = True
 
     @staticmethod
     def init():
-        HotkeyManager._callbacks = []
-        HotkeyManager._bitset = bitarray(2**8)
-        HotkeyManager._bitset.setall(0)
-        HotkeyManager._key_hook = keyboard.hook(
-            HotkeyManager._keyboard_hook_on_event)
-        HotkeyManager._enabled = True
+        HotkeyListener._callbacks = []
+        HotkeyListener._current_bitset = bitarray(2**8)
+        HotkeyListener._current_bitset.setall(0)
+        HotkeyListener._key_hook = pynput.keyboard.Listener(
+            HotkeyListener._keyboard_hook_on_press,
+            HotkeyListener._keyboard_hook_on_release
+        )
+        HotkeyListener._key_hook.start()
+        HotkeyListener._enabled = True
 
     @staticmethod
     def add_hotkey(keys, callback, args):
@@ -152,14 +144,14 @@ class HotkeyManager:
             callback (function): callback to call if the combination is pressed
             args (list): arguments to call callback with
         """
-        bitset = HotkeyManager._keys_to_bitset(keys)
-        HotkeyManager._callbacks.append(HotkeyCallback(bitset, callback, args))
+        HotkeyListener._callbacks.append(
+            KeyboardHotkeyCallback(keys, callback, args))
 
     @staticmethod
     def remove_all():
         """Removes all callbacks and hotkeys
         """
-        HotkeyManager._callbacks.clear()
+        HotkeyListener._callbacks.clear()
 
     @staticmethod
     def set_enabled(b):
@@ -168,31 +160,33 @@ class HotkeyManager:
         Args:
             b (bool): should it work?
         """
-        HotkeyManager._enabled = b
+        HotkeyListener._enabled = b
 
     @staticmethod
-    def _keyboard_hook_on_event(event):
-        code = event.scan_code
-        type = event.event_type
+    def _keyboard_hook_on_press(key):
 
-        if type == "down":
-            HotkeyManager._bitset[code] = True
-            if HotkeyManager._enabled:
-                HotkeyManager._run_matching_hotkey()
+        if isinstance(key, pynput.keyboard.Key):
+            code = key.value.vk
         else:
-            HotkeyManager._bitset[code] = False
+            code = key.vk
+
+        if HotkeyListener._enabled and not HotkeyListener._current_bitset[code]:
+            HotkeyListener._current_bitset[code] = True
+            HotkeyListener._run_matching_hotkey()
+        return True
 
     @staticmethod
-    def _keys_to_bitset(keys):
-        bitmask = bitarray(2**8)
-        bitmask.setall(0)
-        for key in keys:
-            bitmask[key.scan_code] = True
-        return bitmask
+    def _keyboard_hook_on_release(key):
+        if isinstance(key, pynput.keyboard.Key):
+            code = key.value.vk
+        else:
+            code = key.vk
+        HotkeyListener._current_bitset[code] = False
+        return True
 
     @staticmethod
     def _run_matching_hotkey():
-        for callback in HotkeyManager._callbacks:
+        for callback in HotkeyListener._callbacks:
             # Bitmap is matching
-            if HotkeyManager._bitset & callback.key_bitmap == callback.key_bitmap:
+            if HotkeyListener._current_bitset & callback.key_bitmap == callback.key_bitmap:
                 callback.run()
