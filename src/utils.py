05632d57a1e43d98a0d029db5c2849964615b6e0
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-06-28 19:02:42 +0200

    shortened filename for display in table widget

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,10 @@ def check_if_program_present_in_path(program):
     proc = subprocess.Popen(f"where {program}", stdout=subprocess.PIPE)
     output = proc.stdout.read()
     return not output == b''
+
+
+def get_shortened_filename(filename):
+    splitted = filename.split("/")
+    out = ".../" if len(splitted) > 2 else ""
+    out += f"{splitted[-2]}/{splitted[-1]}"
+    return out
