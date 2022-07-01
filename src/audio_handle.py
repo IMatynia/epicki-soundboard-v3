a1bccf2846878f70d89032d6ced270aa45643791
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-07-01 14:49:23 +0200

    audio handle docstrings

--- src/audio_handle.py
+++ src/audio_handle.py
@@ -7,17 +7,31 @@ import numpy as np
 from logging import info
 from os import path
 
-_ALL_AUDIO_THREADS = []
 BLOCK_SIZE = 1024
 
 
 class InvalidDeviceIDError(Exception):
-    def __init__(self, id) -> None:
+    def __init__(self, id: "int") -> None:
         super().__init__(f"Device of id {id} is invalid!")
 
 
 class MultiAudioPlayThread(threading.Thread):
-    def __init__(self, filename, settings: "Settings", args=(), kwargs=None):
+    """Works as a sound file playing thread. Use .start() to make 
+    it run, and .stop_this() to stop at any point. Works with ogg and wav files.
+    Plays the file on 1 or 2 devices, as described in settings.
+    """
+    _ALL_AUDIO_THREADS = []
+
+    def __init__(self, filename: "str", settings: "Settings", args=(), kwargs=None):
+        """Create the mutliaudioplay object.
+
+        Args:
+            filename (str): path to audio file
+            settings (Settings): settings to pull data from
+
+        Raises:
+            FileNotFoundError: if the given file does not exist
+        """
         threading.Thread.__init__(self, args=(), kwargs=None)
         self.daemon = True
         self._stopped = False
@@ -27,7 +41,7 @@ class MultiAudioPlayThread(threading.Thread):
         if not path.exists(self._filename):
             raise FileNotFoundError()
 
-        _ALL_AUDIO_THREADS.append(self)
+        MultiAudioPlayThread._ALL_AUDIO_THREADS.append(self)
 
     def run(self):
         with soundfile.SoundFile(self._filename) as sound_data:
@@ -53,7 +67,7 @@ class MultiAudioPlayThread(threading.Thread):
             secondary_device.start()
 
             for block in sound_data.blocks(blocksize=BLOCK_SIZE, dtype="float32"):
-                processed_block = self.audio_processor(block)
+                processed_block = self._audio_processor(block)
                 secondary_device.write(processed_block)
                 if self._settings.get_play_on_main() and not invalid_device:
                     main_device.write(processed_block)
@@ -71,7 +85,11 @@ class MultiAudioPlayThread(threading.Thread):
         info(f"Stopping sound on {self.name}")
         self._stopped = True
 
-    def audio_processor(self, block_raw):
+    def _audio_processor(self, block_raw):
+        """Post processes the audio before it arrives at audio stream.
+        For the time being all it does is multiplied the volume by a
+        multiplier from settings
+        """
         block = np.empty_like(block_raw)
         block[:] = block_raw
         block *= self._settings.get_volume_multiplier()
@@ -79,14 +97,25 @@ class MultiAudioPlayThread(threading.Thread):
 
 
 def stop_all_sounds():
-    global _ALL_AUDIO_THREADS
-    for thread in _ALL_AUDIO_THREADS:
+    """Stops all running audio threads.
+    """
+    for thread in MultiAudioPlayThread._ALL_AUDIO_THREADS:
         thread.stop_this()
-    _ALL_AUDIO_THREADS = []
+    MultiAudioPlayThread._ALL_AUDIO_THREADS = []
 
 
-def multi_audio_play_async(filename, settings: "Settings"):
+def multi_audio_play_async(filename: "str", settings: "Settings") -> "MultiAudioPlayThread":
+    """Simple function that initializes the MultiAudioPlayThread and returns it
+
+    Args:
+        filename (str): the file to play
+        settings (Settings): settings to use
+
+    Returns:
+        MultiAudioPlayThread: the started audio thread
+    """
     if settings.get_is_singular():
         stop_all_sounds()
     th = MultiAudioPlayThread(filename, settings)
     th.start()
+    return th
