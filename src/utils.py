7456282e0d3bb88ab1f19bfaa864e4561ca939f7
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-07-06 17:31:29 +0200

    utils docstrings and tests

--- src/utils.py
+++ src/utils.py
@@ -15,8 +15,18 @@ def check_if_program_present_in_path(program):
     return not output == b''
 
 
-def get_shortened_filename(filename):
+def get_shortened_filename(filename: "str") -> "str":
+    """Returns a more digestable filename
+
+    Args:
+        filename (str): full path to a file
+
+    Returns:
+        str: shortened path with just one folder preceeding the file
+    """
     splitted = filename.split("/")
+    if len(splitted) == 1:
+        return splitted[0]
     out = ".../" if len(splitted) > 2 else ""
     out += f"{splitted[-2]}/{splitted[-1]}"
     return out
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,15 @@
-from src.utils import check_if_program_present_in_path
-from src.gtts_handle import generate_tts_ogg
+from src.utils import check_if_program_present_in_path, get_shortened_filename
+
 
 def test_check_program():
     assert check_if_program_present_in_path("ffmpeg")
     assert check_if_program_present_in_path("youtube-dl")
     assert not check_if_program_present_in_path("googas")
+
+
+def test_filename_shortner():
+    assert get_shortened_filename("boss baby.mp3") == "boss baby.mp3"
+    assert get_shortened_filename(
+        "some folderino/opa gandam style.wav") == "some folderino/opa gandam style.wav"
+    assert get_shortened_filename(
+        "priest/king/aristocrat/slave.mp4.ogg.wav.ogg") == ".../aristocrat/slave.mp4.ogg.wav.ogg"
