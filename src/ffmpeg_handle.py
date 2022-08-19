09c1e7bbf89e8afd9005cf19823340e0a29684b6
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-20 00:59:34 +0200

    ffmpeg custom params and runtime error exception

--- src/ffmpeg_handle.py
+++ src/ffmpeg_handle.py
@@ -8,13 +8,18 @@ class FFMPEGNotInPathError(Exception):
         super().__init__("FFMPEG was not found in path!")
 
 
-def ffmpeg_conversion(in_file: "str", out_file: "str", params: "str" = None) -> "int":
+class FFMPEGRuntimeError(Exception):
+    def __init__(self) -> None:
+        super().__init__("Runtime FFMPEG error occured")
+
+
+def ffmpeg_conversion(in_file: "str", out_file: "str", params: list = None) -> "int":
     """Converts files between audio-visual formats
 
     Args:
         in_file (str): source path
         out_file (str): destination path
-        params (str, optional): Additional ffmpeg call params. Defaults to None.
+        params (list, optional): Additional ffmpeg call params. Defaults to None.
 
     Raises:
         FFMPEGNotInPathError: if ffmpeg is not available in path
@@ -25,18 +30,22 @@ def ffmpeg_conversion(in_file: "str", out_file: "str", params: "str" = None) -> 
     if not check_if_program_present_in_path("ffmpeg"):
         raise FFMPEGNotInPathError()
 
+    params = [] if params is None else params
+
     info(f"Converting {in_file} to {out_file} with [{params}] parameters")
     command = [
         "ffmpeg",
-        "-y",
-        "-vn",
         "-i",
         f"{in_file}",
+        *params,
+        "-y",
+        "-vn",
         f"{out_file}"
     ]
-
-    if params:
-        command.insert(2, params)
     startupinfo = subprocess.STARTUPINFO()
     startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    return subprocess.Popen(command, startupinfo=startupinfo).wait()
+
+    ret_val = subprocess.Popen(command, startupinfo=startupinfo).wait()
+
+    if not ret_val == 0:
+        raise FFMPEGRuntimeError()
