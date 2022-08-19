d66f02112f1fd62a7437e570d3cf97c0255234f0
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-20 01:01:15 +0200

    custom args support for ytdl download

--- ui/dialog_add_ytdl.py
+++ ui/dialog_add_ytdl.py
@@ -9,7 +9,7 @@ from PySide2.QtWidgets import (
 from src.audio_hotkey import AudioHotkey
 import threading
 from src.youtube_dl_handle import download_media
-from src.ffmpeg_handle import ffmpeg_conversion
+from src.ffmpeg_handle import ffmpeg_conversion, FFMPEGRuntimeError
 from src.constants import DEFAULT_CUSTOM_FOLDER, TEMP_YTDL_FILE
 
 
@@ -32,6 +32,9 @@ class AddYoutubeDialog(QDialog, MessageBoxesInterface):
         custom_name = self._ui.leName.text()
         url = self._ui.leURL.text()
 
+        ytdl_args = self._ui.leYTDLargs.text().split(" ")
+        ffmpeg_args = self._ui.leFFMPEGargs.text().split(" ")
+
         filename = f"{DEFAULT_CUSTOM_FOLDER}/{custom_name}.ogg"
         self._hotkey.set_filename(filename)
         self._hotkey.set_keys(self._scan_button.get_keys())
@@ -47,14 +50,14 @@ class AddYoutubeDialog(QDialog, MessageBoxesInterface):
         else:
             self.setDisabled(True)
             th = threading.Thread(
-                target=self.get_media_async_with_updates, args=[url, filename])
+                target=self.get_media_async_with_updates, args=[url, filename, ytdl_args, ffmpeg_args])
             th.start()
 
     def set_status(self, prompt, percentage):
         self._ui.lStatus.setText(prompt)
         self._ui.progressBar.setValue(percentage)
 
-    def get_media_async_with_updates(self, url, destination):
+    def get_media_async_with_updates(self, url, destination, ytdl_args, ffmpeg_args):
         self.set_status("Starting download", 0)
 
         def callback(line):
@@ -64,10 +67,12 @@ class AddYoutubeDialog(QDialog, MessageBoxesInterface):
                 self.set_status("Downloading media via YT-DL", percentage)
 
         # Download mp3 via YT-DL
-        download_media(url, TEMP_YTDL_FILE, callback)
+        download_media(url, TEMP_YTDL_FILE, ytdl_args, callback)
         # Download finished, FFMPEG conversion
         self.set_status("Converting to OGG via FFMPEG", 95.0)
-        ffmpeg_conversion(TEMP_YTDL_FILE, destination)
+        ret_val = ffmpeg_conversion(TEMP_YTDL_FILE, destination, ffmpeg_args)
+        if not ret_val == 0:
+            raise FFMPEGRuntimeError() 
         remove(TEMP_YTDL_FILE)
         self.on_save_complete()
 
