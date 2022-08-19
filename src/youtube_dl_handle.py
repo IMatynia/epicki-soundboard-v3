f978339fc9f67278fe3b8c157156ac3466903323
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-20 01:00:53 +0200

    ytdl custom args support

--- src/youtube_dl_handle.py
+++ src/youtube_dl_handle.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def download_media(url, destination_file: "str", callback=None):
+def download_media(url, destination_file: "str", custom_args: list = None, callback=None):
     """Downloads the media from given URL and also pipes status into callback.
     This function is blocking. It will await completion of the download.
     The media will be downloaded in MP3 format.
@@ -9,10 +9,14 @@ def download_media(url, destination_file: "str", callback=None):
     Args:
         url (str): media adress
         destination_file (str): destination
+        custom_args (list): list of custom arguments, if none default ones will be used (audio extract)
         callback (function): takes one string argument, the status (optional)
     """
+    custom_args = [] if custom_args is None or custom_args[0] == "" else custom_args
+
     command = [
         "youtube-dl",
+        *custom_args,
         "-x",
         "--audio-format",
         "mp3",
